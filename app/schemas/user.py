c995e1f229d21c7443acc52f36ca1f8c5c1c6b83
seonjeonguk <seonjeongug2@gmail.com>
2025-01-13 22:57:52 +0900

    feat : 유저 로그인 기능 구현

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -5,6 +5,14 @@ class UserPostRequest(BaseModel):
     nickname: str
     password: str
 
+class UserLoginRequest(UserPostRequest):
+    pass
+
+class UserLoginResponse(BaseModel):
+    id: int
+    nickname: str
+    message: str = "Successfully logged in"
+
 
 # 사용자 입력 모델
 class UserInput(BaseModel):
