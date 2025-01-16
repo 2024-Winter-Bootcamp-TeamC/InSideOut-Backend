5473aea7a410c5fd08ce718b8138e55bdf5bcca2
ks9058 <tjdrnr614@naver.com>
2025-01-16 16:57:08 +0900

    Feat: 보고서 생성 기능 수정, 보고서 조회 기능 구현

--- app/schemas/report.py
+++ app/schemas/report.py
@@ -1,27 +1,34 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List, Optional,Dict
 
 class Report(BaseModel):
     report_id: int
     category: str
     title: str
 
-class ReportsResponse(BaseModel):
-    status: str
-    message: str
-    data: Optional[List[Report]]
-
-class ResponseError(BaseModel):
+class ResponseStatus(BaseModel):
     status: str
     message: str
 
-class CreateReport(Report):
+class CreateReport(BaseModel):
+    report_id:int
     user_id:int
     situation_summary:str
+    title:str
     emotion_summary:str
+    category: str
 
-class CreateResponse(BaseModel):
-    status: str
-    message: str
+
+class ReportsResponse(ResponseStatus):
+    data: Optional[List[Report]]
+
+class CreateResponse(ResponseStatus):
     report_id: int
+
+class ReportResponse(ResponseStatus):
+    title:str
+    situation_summary:str
+    emotion_summary: Dict[str, str]
+    wording:str
+    emotion_percentage: Dict[str, float]
 
